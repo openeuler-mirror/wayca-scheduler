1317b96bb10d6ad4298b27ede610122de7cf38c7
young1c <young.yicong@outlook.com>
2021-07-04 08:53:27 +0000

    Add missing LICENSE for Wayca scheduler
    Signed-off-by: Yicong Yang <young.yicong@outlook.com>

--- lib/math.h
+++ lib/math.h
@@ -1,7 +1,20 @@
+/*
+ * Copyright (c) 2021 HiSilicon Technologies Co., Ltd.
+ * Wayca scheduler is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ */
+
 #ifndef LIB_MATH_H
 #define LIB_MATH_H
 /**
- * Mathematic utils. Some are ported from Linux Kernel.
+ * Mathematic utils.
  */
 
 #define min(x, y)	((x) > (y) ? (y) : (x))
